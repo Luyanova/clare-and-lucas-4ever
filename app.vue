42f95bc028183e19168977b9f6e65f519b92d212
Luyanova <lucas.chedhomme424@gmail.com>
2025-06-22 19:34:13 +0200

    first commit

---
+++ app.vue
@@ -0,0 +1,27 @@
+<template>
+  <NuxtLayout>
+    <NuxtRouteAnnouncer />
+    <transition name="page-slide-fade" mode="out-in">
+      <NuxtPage />
+    </transition>
+  </NuxtLayout>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+.page-slide-fade-enter-active, .page-slide-fade-leave-active {
+  transition: opacity 0.4s, transform 0.4s;
+}
+.page-slide-fade-enter-from {
+  opacity: 0;
+  transform: translateX(40px);
+}
+.page-slide-fade-leave-to {
+  opacity: 0;
+  transform: translateX(-40px);
+}
+</style>
+
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="bg-[#fdf5ff] w-screen h-screen flex justify-center items-center">
+        <div class="flex flex-col items-center justify-center gap-7">
+        <h1 class="text-[#02150E] text-4xl ">Projet en construction :)</h1>
+        <div class="flex flex-col lg:flex-row items-center justify-center gap-10">
+        <nuxt-img src="/img/polaUs.png" alt="Pola Us"  />
+        <Logo class="w-50 h-50" />
+
+        </div>
+        </div>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
